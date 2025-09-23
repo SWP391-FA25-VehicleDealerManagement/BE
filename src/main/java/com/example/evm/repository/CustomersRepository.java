1343e2f11844de1c2b2d40cecc659595945f8822
vuongquocd <vuong20032k5@gmail.com>
2025-09-23 18:45:02 +0700

    Add files via upload

---
+++ src/main/java/com/example/evm/repository/CustomersRepository.java
@@ -0,0 +1,10 @@
+package dealermanagementsystem.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dealermanagementsystem.project.entity.Customers;
+@Repository
+public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+
+}
