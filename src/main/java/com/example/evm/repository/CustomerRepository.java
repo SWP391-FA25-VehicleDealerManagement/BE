9ca027746573b88a9a07df24c21d93af457fba2b
vuongquocd <vuong20032k5@gmail.com>
2025-10-01 21:20:56 +0700

    Delete src/main/java/com/example/evm/repository/CustomerRepository.java

--- src/main/java/com/example/evm/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.evm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.evm.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {}
