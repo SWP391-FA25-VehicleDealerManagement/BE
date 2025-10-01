fce5eee50f13abe631fc9053fc606f1ff042e9e0
HuyHG-Z <huynhgiahuy1972@gmail.com>
2025-10-01 21:59:58 +0700

    Create VehicleModel.java

---
+++ src/main/java/com/example/evm/entity/inventory/VehicleModel.java
@@ -0,0 +1,22 @@
+package com.example.evm.model.inventory;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "VehicleModel")
+public class VehicleModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer model_id;
+
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "model")
+    private List<VehicleVariant> variants;
+}
