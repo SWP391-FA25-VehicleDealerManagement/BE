9db694badd298ec1d092caf5ea0d6a911058b065
HuyHG-Z <huynhgiahuy1972@gmail.com>
2025-10-01 22:00:34 +0700

    Create VehicleVariant.java

---
+++ src/main/java/com/example/evm/entity/inventory/VehicleVariant.java
@@ -0,0 +1,32 @@
+package com.example.evm.model.inventory;
+
+import com.example.evm.model.vehicle.Vehicle;
+import com.example.evm.model.order.SalePrice;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "VehicleVariant")
+public class VehicleVariant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer variant_id;
+
+    private String name;
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id")
+    private VehicleModel model;
+
+    @OneToMany(mappedBy = "variant")
+    private List<Vehicle> vehicles;
+
+    @OneToMany(mappedBy = "variant")
+    private List<SalePrice> salePrices;
+}
