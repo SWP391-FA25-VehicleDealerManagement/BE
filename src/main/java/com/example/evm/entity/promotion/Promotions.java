3231b66086e4bc6cd97204841a905fd810a342cb
HuyHG-Z <huynhgiahuy1972@gmail.com>
2025-10-01 22:09:27 +0700

    Create Promotions.java

---
+++ src/main/java/com/example/evm/entity/promotion/Promotions.java
@@ -0,0 +1,39 @@
+package com.example.evm.model.promotion;
+
+import com.example.evm.model.dealer.Dealer;
+import com.example.evm.model.order.OrderDetail;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Promotions")
+public class Promotions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer promo_id;
+
+    private String title;
+    private String description;
+    private Double discountRate;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "dealer_id")
+    private Dealer dealer;
+
+    @OneToMany(mappedBy = "promo")
+    private List<PromotionDealer> promotionDealers;
+
+    @OneToMany(mappedBy = "promo")
+    private List<PromotionVehicle> promotionVehicles;
+
+    @OneToMany(mappedBy = "promotion")
+    private List<OrderDetail> orderDetails;
+}
