f87437e2eb25ae87b1b75877dd1a4b05418da7b1
vuongquocd <vuong20032k5@gmail.com>
2025-10-02 17:43:20 +0700

    Create CustomerInfo.java

---
+++ src/main/java/com/example/evm/dto/customer/CustomerInfo.java
@@ -0,0 +1,18 @@
+package com.example.evm.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerInfo {
+    private Integer customerId;
+    private String customerName;
+    private String email;
+    private String phone;
+    private Integer dealerId;
+}
