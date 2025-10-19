4cc7a0ca09b17c3fa29c4644618b09cdb5863ab4
vuongquocd <vuong20032k5@gmail.com>
2025-10-19 20:16:30 +0700

    Update CustomerRepository.java

--- src/main/java/com/example/evm/repository/customer/CustomerRepository.java
+++ src/main/java/com/example/evm/repository/customer/CustomerRepository.java
@@ -1,10 +1,14 @@
 package com.example.evm.repository.customer;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.evm.entity.customer.Customer;
 
 public interface CustomerRepository  extends JpaRepository<Customer,Long>{
 
+    List<Customer> findByDealerId(Long dealerId);
     
 } 
