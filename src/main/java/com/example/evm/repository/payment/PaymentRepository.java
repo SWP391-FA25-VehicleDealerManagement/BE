79285bc454274be04c77a26a0d7f47cbb1dd7fc4
vuongquocd <vuong20032k5@gmail.com>
2025-10-22 19:48:20 +0700

    Update PaymentRepository.java

--- src/main/java/com/example/evm/repository/payment/PaymentRepository.java
+++ src/main/java/com/example/evm/repository/payment/PaymentRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.evm.entity.payment.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long>{
+Optional<Payment> findByOrderId(Long orderId);
 
 }
