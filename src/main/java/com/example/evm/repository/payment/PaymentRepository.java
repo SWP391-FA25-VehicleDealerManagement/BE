0d6fcc321044ca4919a8cefc10514eb59112b3f8
truongnduy <truongnduy1808@gmail.com>
2025-10-31 22:54:25 +0700

    fix conflict

--- src/main/java/com/example/evm/repository/payment/PaymentRepository.java
+++ src/main/java/com/example/evm/repository/payment/PaymentRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.evm.entity.payment.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long>{
-Optional<Payment> findById(Long paymentId);
-
+Optional<Payment> findByOrderId(Long orderId);
 }
